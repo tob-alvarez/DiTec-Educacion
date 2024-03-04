78abc7dc15d22eb31726a9d9bc9f9b5a16962d0d
fabyrios <fabianarios27@gmail.com>
2024-03-04 12:35:15 -0300

    Pestaña de Establecimientos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "./common/Layout";
 import Home from '../src/components/Home/Home'
 import Autoridades from "./components/Autoridades/Autoridades";
+import Establecimientos from "./components/Establecimientos/Establecimientos";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route exact path="/*" element={<Home />} />
             <Route exact path="/home" element={<Home />} />
             <Route exact path="/autoridades" element={<Autoridades />} />
+            <Route exact path="/establecimientos" element={<Establecimientos />} />
             {/* <Route exact path="/perfil" element={<Perfil />} /> */}
           </Routes>
         </Layout>
