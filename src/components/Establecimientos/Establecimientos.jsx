3cdd5e323a61435fbdb54f9c6c84e264740acaa4
tob-alvarez <tosal1099@gmail.com>
2024-03-05 10:22:47 -0300

    Padrones
    listo padrones

--- src/components/Establecimientos/Establecimientos.jsx
+++ src/components/Establecimientos/Establecimientos.jsx
@@ -1,41 +1,17 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+/* eslint-disable react/no-unescaped-entities */
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+
 
 export default function Establecimientos() {
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+  
   return (
     <div className='container d-flex flex-wrap gap-3 mt-5 mb-5'>
-    <Card sx={{ maxWidth: 400 }}>
+    <Card sx={{ maxWidth: 350 }}>
       <CardHeader style={{backgroundColor: "#0A82B4", textAlign: "center" }} 
         title="Nivel Inicial"
       />
@@ -80,7 +56,7 @@ export default function Establecimientos() {
       </CardContent>
     </Card>
 
-    <Card sx={{ maxWidth: 400 }}>
+    <Card sx={{ maxWidth: 350 }}>
       <CardHeader style={{backgroundColor: "#F1EA10", textAlign: "center" }} 
         title="Nivel Primario"
       />
@@ -103,7 +79,7 @@ export default function Establecimientos() {
       </CardContent>
     </Card>
 
-    <Card sx={{ maxWidth: 400 }}>
+    <Card sx={{ maxWidth: 350 }}>
       <CardHeader style={{backgroundColor: "#07C85F", textAlign: "center" }} 
         title="Nivel Secundario"
       />
